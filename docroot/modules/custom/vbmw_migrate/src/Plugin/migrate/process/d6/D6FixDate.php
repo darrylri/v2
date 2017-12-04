5e8059a9248d01918b39f97f813711ad4e5483a1
darrylri <darrylri@exmsft.com>
2017-12-04 06:01:41 -0800

    Add news and people content type migrations, d6 file data and date fixing process plugins, and pathauto patterns for content types.

--- docroot/modules/custom/vbmw_migrate/src/Plugin/migrate/process/d6/D6FixDate.php
+++ docroot/modules/custom/vbmw_migrate/src/Plugin/migrate/process/d6/D6FixDate.php
@@ -1,29 +1,31 @@
 <?php
+
 namespace Drupal\vbmw_migrate\Plugin\migrate\process\d6;
 
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
-/**
- * Maps a set of D6 field values to address values.
- *
- * @MigrateProcessPlugin(
- *   id = "d6_file_data"
- * )
- */
+  /**
+   * Forces various date formats into one that can be inserted into the db.
+   *
+   * @MigrateProcessPlugin(
+   *   id = "d6_fix_date"
+   * )
+   */
 
-class D6FileData extends ProcessPluginBase {
+class D6FixDate extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $format = 'Y-m-d h:i:s';
+    if (!empty($this->configuration['format'])) {
+      $format = $this->configuration['format'];
+    }
     if (!empty($value) && is_string($value)) {
-      $data = unserialize($value);
-      foreach ($data as $key => $item) {
-        $row->setDestinationProperty($key, $item);
-      }
+      $value = date($format, strtotime($value));
     }
 
     return $value;
